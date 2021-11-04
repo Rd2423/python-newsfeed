ff62ed57eec43176152bdb311aaf6159d9ef10d0
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-11-04 00:58:13 -0400

    added logged in decorater function to make sure that client is connected before doing any operation on the page

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,9 +1,12 @@
 import sys
 from flask import Blueprint, json, request, jsonify, session
-from app.models import User
+from app.models import User, Post, Comment, Vote
 from app.db import get_db
+from app.utils.auth import login_required
 
 bp = Blueprint('api', __name__, url_prefix='/api')
+
+# Users API
 
 
 @bp.route('/users', methods=['POST'])
@@ -43,16 +46,134 @@ def login():
         return jsonify(message='Incorrect credentials'), 400
 
     if user.verify_password(data['password']) == False:
-        return jsonify(message = 'Incorrect credentials'), 400
-    
+        return jsonify(message='Incorrect credentials'), 400
+
     session.clear()
     session['user_id'] = user.id
     session['loggedIn'] = True
 
-    return jsonify(id = user.id)
+    return jsonify(id=user.id)
+
 
 @bp.route('/users/logout', methods=['POST'])
 def logout():
     # remove session variables
     session.clear()
     return '', 204
+
+########################################################################
+
+# Comment
+
+
+@bp.route('/comments', methods=['POST'])
+@login_required
+def comment():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # Create a new comment
+        newComment = Comment(
+            comment_text=data['comment_text'],
+            post_id=data['post_id'],
+            user_id=session.get('user_id')
+        )
+        db.add(newComment)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message='Comment Failed'), 500
+
+    return jsonify(id=newComment.id)
+##################################################################################################
+
+# Up vote route
+
+
+@bp.route('/posts/upvote', methods=['PUT'])
+@login_required
+def upvote():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # create a new vote with incoming id and session id
+        newVote = Vote(
+            post_id=data['post_id'],
+            user_id=session.get('user_id')
+        )
+
+        db.add(newVote)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message='Upvote failed'), 500
+
+    return '', 204
+
+    ###############################################################################################
+
+@bp.route('/posts', methods=['POST'])
+@login_required
+def create():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+            # create a new post
+        newPost = Post(
+            title=data['title'],
+            post_url=data['post_url'],
+            user_id=session.get('user_id')
+        )
+
+        db.add(newPost)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message='Post failed'), 500
+
+    return jsonify(id=newPost.id)
+
+
+@bp.route('/posts/<id>', methods=['PUT'])
+@login_required
+def update(id):
+    # data is the data that we received from the user
+    data = request.get_json()
+    db = get_db()
+    try:
+        post = db.query(Post).filter(Post.id == id).one()
+        post.title = data['title']
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message = 'Post not found'), 404
+    
+    return '', 204
+
+@bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
+def delete(id):
+  db = get_db()
+
+  try:
+    # delete post from db
+    db.delete(db.query(Post).filter(Post.id == id).one())
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Post not found'), 404
+
+  return '', 204
